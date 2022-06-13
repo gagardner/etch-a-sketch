e7720527292f1d363a0a0707911afd0e2c321cd5
gagardner <glngrdnr@gmail.com>
2022-06-13 15:00:47 +0200

    Fix grid to fixed size, grid now updates on user input

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 let gridContainer = document.getElementById("grid-container");
+let gridButton = document.getElementById("grid-button");
+let userInput = document.getElementById("quantity");
 
-for (let i = 1; i <= 16 * 16; i++) {
+function createSquares(n) {
+    if (n > 75) n = 75;
+    for (let i = 1; i <= n * n; i++) {
     let square = document.createElement("div");
-    square.classList.add("grid-square");
-    //para.innerHTML = i;
-    gridContainer.appendChild(square);
-    square.addEventListener("mouseenter", function () {
-        square.setAttribute("style", "background-color: yellow");
+    square.classList.add("grid-square");   
+    gridContainer.appendChild(square);     
+    }
+}
+
+function makeSquaresInteractive() {
+    const squares = document.querySelectorAll(".grid-square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", function () {
+            square.setAttribute("style", "background-color: green");
+        });
     });
 }
-/*
-const squares = document.querySelectorAll(".grid-square");
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", function () {
-        square.setAttribute("style", "background-color: green");
-    });
-}); */
+
+createSquares(16);
+makeSquaresInteractive();
+
+
+updateGrid = () => {
+    if (userInput.value > 75) userInput.value = 75;
+    gridContainer.innerHTML = "";
+    gridContainer.style.setProperty(
+        "grid-template-columns",
+        `repeat(${userInput.value}, 1fr)`
+    );
+    gridContainer.style.setProperty(
+        "grid-template-rows",
+        `repeat(${userInput.value}, 1fr)`
+    );
+
+    createSquares(userInput.value);
+    makeSquaresInteractive();
+};
+
+userInput.addEventListener("change", updateGrid);
+
+
+
+
